c1379c7b30f0df42560d76fc56930757cf52e368
Agbropro <nicholasdimas15@gmail.com>
2026-02-01 21:15:20 +0700

    integrated : integrates all folder from learning steps : 1.database_structure 2. interface layer, 3. client scripts simulating detection and injecting to database

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# MAIN APP
+
+```python
+python3 -m uvicorn integrated.app.main:app
+```
+
+
+# CLIENT
+
+```python
+python3 -m integrated.scripts.camera_client
+```
---
+++ integrated/app/main.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+from fastapi import FastAPI
+from ..internal.client.database import Base, engine
+from ..internal.delivery.routes import router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="MY AI SERVER")
+app.include_router(router)
